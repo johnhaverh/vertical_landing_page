59c655c98e1af26974f5f3874f8e38f653d040c0
John Haver Herrera <johnhaverh@gmail.com>
2023-08-29 18:17:50 -0500

    configure routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/router/router.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+   Flurorouter.configureRoutes();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Container()
+      title: 'Landing page App',
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
+      //home: 
     );
   }
 }
