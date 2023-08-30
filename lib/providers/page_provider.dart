9e866736a06f8ad1b7b595f868126ccafee8cd82
John Haver Herrera <johnhaverh@gmail.com>
2023-08-30 14:53:14 -0500

    add functionality for url name & listener

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -5,13 +5,29 @@ import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
 
-  PageController scrollController = new PageController();
+  PageController scrollController = PageController();
 
-  List<String> _pages = ['home','about', 'pricing','contact','location'];
+  final List<String> _pages = ['home','about', 'pricing','contact','location'];
+  int _currentIndex= 0;
+
+  createScrollController(String routeName){
+    scrollController = PageController(initialPage: getPageIndex(routeName));
+    scrollController.addListener(() {
+      final index = (scrollController.page ?? 0).round();
+      if(index != _currentIndex) {
+        html.window.history.pushState(null, '', '#/${_pages[index]}' );
+        _currentIndex = index;
+      }
+    });
+  }
+
+  getPageIndex(String routeName){
+    return _pages.indexOf(routeName) == -1 ? 0 : _pages.indexOf(routeName);
+  }
 
   goTo(int index){
     //final routeName = _pages[index];
-    html.window.history.pushState(null, '', '#/${_pages[index]}' );
+    //html.window.history.pushState(null, '', '#/${_pages[index]}' );
     scrollController.animateToPage(
       index, 
       duration: Duration(milliseconds: 300), 
