274d4c0a686bd235cf82c0065b61b75ca7b3dcd3
John Haver Herrera <johnhaverh@gmail.com>
2023-08-30 07:56:02 -0500

    add new views

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
    Flurorouter.configureRoutes();
   }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/views/about_view.dart';
+import 'package:vertical_landing_page/ui/views/contact_view.dart';
+import 'package:vertical_landing_page/ui/views/home_view.dart';
+import 'package:vertical_landing_page/ui/views/location_view.dart';
+import 'package:vertical_landing_page/ui/views/pricing_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.red,
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            _HomeBody(),
+
+            Positioned(
+              right: 20,
+              top:20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView(),
+      ],
     );
   }
 }
